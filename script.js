826df9ffcb0a477ea8655444f34324e81d6ff028
cosmonet-fr <stephane.billois@hotmail.com>
2020-04-28 16:22:46 +0200

    Ajout des images par défaut de l'API.

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 
 var meteoTxt = document.getElementById('meteoTxt');
+var imgMeteo = document.getElementById('imgMeteo')
 var request = new XMLHttpRequest();
 request.onreadystatechange = function() {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
         var response = JSON.parse(this.responseText);
         console.log('Le temps est ' + response.current_condition.condition + '.');
+        imgMeteo.setAttribute("src", response.current_condition.icon_big);
         switch (response.current_condition.condition) {
           case 'Ensoleillé':
-              meteoTxt.innerHTML = 'Le temps est ensoleillé. On peut aller faire une belle promeunade mon bébé <3';
+              meteoTxt.innerHTML = 'Le temps est ensoleillé. On peut aller faire une belle promenade mon bébé. <3';
             break;
 
           case 'Nuit claire':
               meteoTxt.innerHTML = 'La nuit est claire.';
             break;
           case 'Eclaircies':
-              meteoTxt.innerHTML = 'Il y a des éclaircies. On peut aller faire une belle promeunade mon bébé <3';
+              meteoTxt.innerHTML = 'Il y a des éclaircies. On peut aller faire une belle promenade mon bébé. <3';
             break;
 
           default:
